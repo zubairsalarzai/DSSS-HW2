aa20c519d90b4501fa4381279c2536c26839ba16
zubairsalarzai <zubairsalar.hrp@gmail.com>
2023-11-16 13:11:37 +0100

    error removed and methods name change

--- math_quiz.py
+++ math_quiz.py
@@ -1,40 +1,34 @@
 import random
-
-
-def function_A(min, max):
-    """
-    Random integer.
-    """
+def Operands(min, max):
+ #here getting random integer
     return random.randint(min, max)
-
-
-def function_B():
+def Operater():
     return random.choice(['+', '-', '*'])
-
-
-def function_C(n1, n2, o):
+def Calculation(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
-
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    't_q = 3.14159265359'
+    t_q = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = Operands(1, 10);
+        n2 = Operands(1, 5);
+        o = Operater();
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Calculation(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
+# useranswer is str type i converted in int
+        if int(useranswer) == ANSWER:
             print("Correct! You earned a point.")
             s += -(-1)
         else:
