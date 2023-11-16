b1edaf9ef02dc5c960e9d7c2e5dcae8c9ae3dbc1
zubairsalarzai <zubairsalar.hrp@gmail.com>
2023-11-16 14:44:58 +0100

    setup file change

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='DSSS-HW2',
+    version='0.1',
+    description='Your package description',
+    author='Zubair Salarzai',
+    author_email='zubairsalar.hrp@gmail.com',
+    url='https://github.com/zubairsalarzai/DSSS-HW2.git',
+    packages=['random','unittest'],  # Replace 'your_package' with the actual package name
+    install_requires=[
+        # List your dependencies here
+    ],
+)
